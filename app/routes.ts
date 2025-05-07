af788a33209ec8ad88d778561d2c62c1cdb5f017
F. Ricardo <112594292+FelipeRicardo5@users.noreply.github.com>
2025-05-07 14:25:19 +0000

    fix: implementando rotas em routes.ts

--- app/routes.ts
+++ app/routes.ts
@@ -1,12 +1,16 @@
-import { type RouteConfig, 
-    index, 
+import {
+    type RouteConfig,
+    index,
     route,
     layout,
     prefix,
 } from "@react-router/dev/routes";
 
 export default [
-    index("routes/login/index.tsx"),   
-    route("cases","routes/cases/index.jsx"),
+    index("routes/login/index.tsx"),
+
+    layout("./components/layouts/layout.jsx", [
+        route("cases", "routes/cases/index.jsx"),
+    ]),
 
 ] satisfies RouteConfig;
